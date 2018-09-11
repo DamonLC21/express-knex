56147c051245a8132def13ae9913ae037ec6ea33
Damon Chivers <chiversdamon@gmail.com>
2018-09-11 09:25:10 -0600

    response working

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
+app.get('/', (require,response) => {
+    response.send('The Route Worked!')
+})
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
